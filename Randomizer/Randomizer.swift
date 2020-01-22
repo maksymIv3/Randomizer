9caa759038933874bf59289b48bd64c214fff2f7
Maksym Ivanyk <maksym.ivanyk@rolique.io>
2020-01-22 14:58:04 +0200

    Added test cases

---
+++ Randomizer/Randomizer.swift
@@ -0,0 +1,21 @@
+//
+//  Randomizer.swift
+//  Randomizer
+//
+//  Created by Maksym Ivanyk on 22.01.2020.
+//  Copyright © 2020 Maksym Ivanyk. All rights reserved.
+//
+
+import Foundation
+
+public final class Randomizer {
+    let name = "Randomizer"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- RandomizerTests/RandomizerTests.swift
+++ RandomizerTests/RandomizerTests.swift
@@ -11,24 +11,18 @@ import XCTest
 
 class RandomizerTests: XCTestCase {
 
+    var randomizer: Randomizer!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        randomizer = Randomizer()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(randomizer.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+        XCTAssertEqual(randomizer.sub(a: 2, b: 1), 1)
     }
 
 }
